276c1620c658426d0eef791ac668732b8763f582
Johannes Hetzl Svensson <johannes.hetzlsvens@learnet.se>
2017-10-04 13:21:19 +0200

    fahad!!!

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-function calc() {
-	var valuta 1 = parseInt(document.getElementById("val1").value);
-	var type = document.getElementById("type").value;
-	var valuta 2 = parseInt(document.getElementById("val2").value);
-	if(type =="plus") {
-	document.getElementById("out").innerHTML = valuta 1+valuta 2;
+function omvandla() {
+	var valuta1 = parseInt(document.getElementById("valuta 1").value);
+	var type1 = document.getElementById("type1").value;
+	var type2 = document.getElementById("type2").value;
+	var valuta2  = parseInt(document.getElementById("valuta 2").value);
+	var inp = valuta1;
+	
+	
+	if(type1 =="USD") {
+		inp *= 8;
 	}
+	if (type2 =="")
+	
+	
+	
+	
+	
 	if(type=="minus"){
-	document.getElementById("out").innerHTML = valuta 1-valuta 2;
+	document.getElementById("out").innerHTML = valuta1-valuta2;
 	}
 	if(type=="divide"){
-	document.getElementById("out").innerHTML = valuta 1/valuta 2;
+	document.getElementById("out").innerHTML = valuta1/valuta2;
 	}
 	if(type=="multi"){
-	document.getElementById("out").innerHTML = valuta 1*valuta 2;
+	document.getElementById("out").innerHTML = valuta1*valuta2;
 	}
 	if(type=="pow"){
-	document.getElementById("out").innerHTML = Math.pow (valuta 1, valuta 2);
+	document.getElementById("out").innerHTML = Math.pow (valuta1, valuta2);
 	}
 }
