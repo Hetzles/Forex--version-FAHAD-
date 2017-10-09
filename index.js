33c599a86ecf3e1c4a2a13ab8a9205ccb156971b
Johannes Hetzl Svensson <johannes.hetzlsvens@learnet.se>
2017-10-09 14:49:11 +0200

    boi work

--- index.js
+++ index.js
@@ -2,34 +2,48 @@ function omvandla() {
 	var valuta1 = parseInt(document.getElementById("valuta 1").value);
 	var type1 = document.getElementById("type1").value;
 	var type2 = document.getElementById("type2").value;
-	var valuta2  = parseInt(document.getElementById("valuta 2").value);
 	var inp = valuta1;
 	
 	
 	if(type1 =="USD") {
-		inp *= 8;
+		inp *= 8.1;
 	}
 	if (type1 =="EUR") {
-		inp
+		inp *= 9.5;
+	}
+	if (type1 =="SEK"){
+		inp *= 1;
+	}
+	if (type1 =="INR"){
+		inp /= 8.03;
+	}
+	if (type1 == "SAR"){
+		inp *= 2.1;
+	}
+	if (type1 == "GBP"){
+		inp *= 10.7;
+	}
+	//type 2
+	
+	if (type2 == "USD"){
+		inp /= 8.1;
+	}
+	if (type2 =="EUR") {
+		inp/= 9.5;
+	}
+	if (type2 =="SEK"){
+		inp /= 1;
+	}
+	if (type2 =="INR"){
+		inp *= 8.03;
+	}
+	if (type2 == "SAR"){
+		inp /= 2.1;
+	}
+	if (type2 == "GBP"){
+		inp /= 10.7;
 	}
 	
-	
-	if (type2 =="USD")
-	
-	
-	
-	
-	
-	if(type=="minus"){
-	document.getElementById("out").innerHTML = valuta1-valuta2;
-	}
-	if(type=="divide"){
-	document.getElementById("out").innerHTML = valuta1/valuta2;
-	}
-	if(type=="multi"){
-	document.getElementById("out").innerHTML = valuta1*valuta2;
-	}
-	if(type=="pow"){
-	document.getElementById("out").innerHTML = Math.pow (valuta1, valuta2);
-	}
+	document.getElementById("valuta 2").value = inp;
 }
+	
